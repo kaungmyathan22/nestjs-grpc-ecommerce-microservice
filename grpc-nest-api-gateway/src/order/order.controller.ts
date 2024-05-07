b73a33fb8f2a86274cf6a4c026af3d5760b538ea
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-07 15:24:19 +0630

    feat(gateway): product & auth & order setup finished

--- grpc-nest-api-gateway/src/order/order.controller.ts
+++ grpc-nest-api-gateway/src/order/order.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Inject,
+  OnModuleInit,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+import { AuthGuard } from '../auth/auth.guard';
+import {
+  CreateOrderRequest,
+  CreateOrderResponse,
+  ORDER_SERVICE_NAME,
+  OrderServiceClient,
+} from './order.pb';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController implements OnModuleInit {
+  private svc: OrderServiceClient;
+
+  @Inject(ORDER_SERVICE_NAME)
+  private readonly client: ClientGrpc;
+
+  public onModuleInit(): void {
+    this.svc = this.client.getService<OrderServiceClient>(ORDER_SERVICE_NAME);
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  private async createOrder(
+    @Req() req: Request,
+  ): Promise<Observable<CreateOrderResponse>> {
+    const body: CreateOrderRequest = req.body;
+
+    body.userId = <number>(req as any).user;
+
+    return this.svc.createOrder(body);
+  }
+}
