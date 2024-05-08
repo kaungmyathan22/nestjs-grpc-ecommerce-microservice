80b060d5ab77f55afc4cde3479653ee0da0a1031
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-08 12:29:51 +0630

    feat(order): order module setup finished

--- grpc-nest-order-svc/src/app.module.ts
+++ grpc-nest-order-svc/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [OrderModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5123,
+      database: 'micro_order',
+      username: 'admin',
+      password: 'admin',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+    }),
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
